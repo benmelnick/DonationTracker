c7c931bbb40bbbf640ac755a7a9e5087a0b4de78
Josh-McFarlin <jmcfarlin6@gatech.edu>
2018-10-10 21:48:48 -0400

    Updated list core

--- app/src/main/java/com/example/benmelnick/donationtracker/LocationListActivity.java
+++ app/src/main/java/com/example/benmelnick/donationtracker/LocationListActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
@@ -18,19 +19,23 @@ import android.widget.TextView;
 import java.util.List;
 
 public class LocationListActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_list);
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
 
-        View recyclerView = findViewById(R.id.list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
-    }
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new MyAdapter(Model.INSTANCE.getItems()));
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(Model.INSTANCE.getItems());
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
@@ -40,10 +45,10 @@ public class LocationListActivity extends AppCompatActivity {
         // Complex data items may need more than one view per item, and
         // you provide access to all the views for a data item in a view holder
         public class MyViewHolder extends RecyclerView.ViewHolder {
-            public TextView mTextView;
-            public MyViewHolder(TextView v) {
+            public View mView;
+            public MyViewHolder(View v) {
                 super(v);
-                mTextView = v;
+                mView = v;
             }
         }
 
@@ -54,13 +59,11 @@ public class LocationListActivity extends AppCompatActivity {
 
         // Create new views (invoked by the layout manager)
         @Override
-        public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                         int viewType) {
+        public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             // create a new view
-            TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.activity_location_list, parent, false);
-            MyViewHolder vh = new MyViewHolder(v);
-            return vh;
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.location_list_item, parent, false);
+            return new MyViewHolder(v);
         }
 
         // Replace the contents of a view (invoked by the layout manager)
@@ -68,7 +71,7 @@ public class LocationListActivity extends AppCompatActivity {
         public void onBindViewHolder(MyViewHolder holder, int position) {
             // - get element from your dataset at this position
             // - replace the contents of the view with that element
-            holder.mTextView.setText(mItems.get(position).getName());
+            holder.mView.setText(mItems.get(position).getName());
         }
 
         // Return the size of your dataset (invoked by the layout manager)
